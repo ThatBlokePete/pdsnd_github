3dbea1ebdd258451e20ebce8216795a2768a1294
Peter Garrett <Pete.Garrett@me.com>
2019-11-06 16:40:58 +0100

    BikeShare.py repeat loop syntax modified to match other while loops

--- BikeShare.py
+++ BikeShare.py
@@ -290,8 +290,9 @@ def main():
         user_stats(df)
         raw_data(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
-        if restart.lower() != 'yes':
+        print('\nwould you like to start from the beginning?')
+        restart = input('y or n\n')
+        if restart.lower() == 'n':
             break
 
 
